2e797807ba3cadbf3e0d5ad94d9ef0700e802c1f
ilumi <ilumi@LAPTOP-9RJHQ11J>
2022-07-03 22:16:46 +0200

    Dodaj pliki projektów.

---
+++ DBBackup/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace DBBackup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.txtDataSource = new System.Windows.Forms.TextBox();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbDatabases = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtBackupFileLoc = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnBrowse2 = new System.Windows.Forms.Button();
+            this.txtRestoreFileLoc = new System.Windows.Forms.TextBox();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtUserId);
+            this.groupBox1.Controls.Add(this.txtDataSource);
+            this.groupBox1.Controls.Add(this.btnDisconnect);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(780, 123);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SQL Server Authentication";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(416, 70);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(216, 23);
+            this.txtPassword.TabIndex = 7;
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Location = new System.Drawing.Point(111, 70);
+            this.txtUserId.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(208, 23);
+            this.txtUserId.TabIndex = 6;
+            // 
+            // txtDataSource
+            // 
+            this.txtDataSource.Location = new System.Drawing.Point(111, 25);
+            this.txtDataSource.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDataSource.Name = "txtDataSource";
+            this.txtDataSource.Size = new System.Drawing.Size(335, 23);
+            this.txtDataSource.TabIndex = 5;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(655, 66);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(100, 28);
+            this.btnDisconnect.TabIndex = 4;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(655, 25);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(100, 28);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(337, 74);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 77);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "User ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Source:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbDatabases);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(17, 146);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(779, 123);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Database Selection";
+            // 
+            // cmbDatabases
+            // 
+            this.cmbDatabases.FormattingEnabled = true;
+            this.cmbDatabases.Location = new System.Drawing.Point(109, 28);
+            this.cmbDatabases.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbDatabases.Name = "cmbDatabases";
+            this.cmbDatabases.Size = new System.Drawing.Size(404, 24);
+            this.cmbDatabases.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 32);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Database";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnBackup);
+            this.groupBox3.Controls.Add(this.btnBrowse);
+            this.groupBox3.Controls.Add(this.txtBackupFileLoc);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(17, 278);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox3.Size = new System.Drawing.Size(779, 123);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Database Backup";
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Location = new System.Drawing.Point(557, 68);
+            this.btnBackup.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(100, 28);
+            this.btnBackup.TabIndex = 3;
+            this.btnBackup.Text = "Backup";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(557, 32);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(100, 28);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtBackupFileLoc
+            // 
+            this.txtBackupFileLoc.Location = new System.Drawing.Point(109, 36);
+            this.txtBackupFileLoc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtBackupFileLoc.Name = "txtBackupFileLoc";
+            this.txtBackupFileLoc.Size = new System.Drawing.Size(404, 23);
+            this.txtBackupFileLoc.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 36);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Location";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 36);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Backup Path";
+            // 
+            // btnBrowse2
+            // 
+            this.btnBrowse2.Location = new System.Drawing.Point(557, 32);
+            this.btnBrowse2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBrowse2.Name = "btnBrowse2";
+            this.btnBrowse2.Size = new System.Drawing.Size(100, 28);
+            this.btnBrowse2.TabIndex = 4;
+            this.btnBrowse2.Text = "Browse";
+            this.btnBrowse2.UseVisualStyleBackColor = true;
+            this.btnBrowse2.Click += new System.EventHandler(this.btnBrowse2_Click);
+            // 
+            // txtRestoreFileLoc
+            // 
+            this.txtRestoreFileLoc.Location = new System.Drawing.Point(109, 32);
+            this.txtRestoreFileLoc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtRestoreFileLoc.Name = "txtRestoreFileLoc";
+            this.txtRestoreFileLoc.Size = new System.Drawing.Size(404, 23);
+            this.txtRestoreFileLoc.TabIndex = 1;
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Location = new System.Drawing.Point(557, 68);
+            this.btnRestore.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(100, 28);
+            this.btnRestore.TabIndex = 5;
+            this.btnRestore.Text = "Restore";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.btnRestore);
+            this.groupBox4.Controls.Add(this.txtRestoreFileLoc);
+            this.groupBox4.Controls.Add(this.btnBrowse2);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Location = new System.Drawing.Point(17, 410);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox4.Size = new System.Drawing.Size(779, 123);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Database Restore";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 554);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Database Backup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.TextBox txtDataSource;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbDatabases;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtBackupFileLoc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnBrowse2;
+        private System.Windows.Forms.TextBox txtRestoreFileLoc;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.GroupBox groupBox4;
+    }
+}
+
---
+++ DBBackup/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DBBackup
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn;
+        private SqlCommand command;
+        private SqlDataReader reader;
+        string connectionString = "";
+        string sql = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //connecting to server
+                connectionString = "Data Source = " + txtDataSource.Text + "; User Id = " + txtUserId.Text + "; Password = " + txtPassword.Text + ""; 
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+                //receving list of existing databases
+                sql = "EXEC sp_databases";
+                command = new SqlCommand(sql, conn);
+                reader = command.ExecuteReader();
+                cmbDatabases.Items.Clear();
+                while (reader.Read())
+                {
+                    cmbDatabases.Items.Add(reader[0].ToString());
+                }
+                reader.Dispose();
+                conn.Close();
+                conn.Dispose();
+
+                //disabling/enabling labels and buttons
+                txtDataSource.Enabled = false;
+                txtUserId.Enabled = false;
+                txtPassword.Enabled = false;
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+
+                btnBackup.Enabled = true;
+                btnRestore.Enabled = true;
+
+                cmbDatabases.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            //disabling/enabling labels and buttons
+            txtDataSource.Enabled = true;
+            txtUserId.Enabled = true;
+            txtPassword.Enabled = true;
+            cmbDatabases.Enabled = false;
+            btnBackup.Enabled = false;
+            btnRestore.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //disabling/enabling labels and buttons
+            btnDisconnect.Enabled = false;
+            cmbDatabases.Enabled = false;
+            btnBackup.Enabled = false;
+            btnRestore.Enabled = false;
+        }
+
+        private void btnBackup_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(cmbDatabases.Text.CompareTo("") == 0)
+                {
+                    MessageBox.Show("Please select database");
+                    return;
+                }
+                //establishing connection
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                //preparing and executing backup query
+                sql = "BACKUP DATABASE " + cmbDatabases.Text + " TO DISK = '" + txtBackupFileLoc.Text + "\\" + cmbDatabases.Text + "-" + DateTime.Now.Ticks.ToString() + ".bak'";
+                command = new SqlCommand(sql, conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+                conn.Dispose();
+                MessageBox.Show("Backup complete");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dlg = new FolderBrowserDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtBackupFileLoc.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void btnBrowse2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Backup Files(*.bak)|*.bak|All Files(*.*)|*.*";
+            dlg.FilterIndex = 0;
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtRestoreFileLoc.Text = dlg.FileName;
+            }
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cmbDatabases.Text.CompareTo("") == 0)
+                {
+                    MessageBox.Show("Please select database");
+                    return;
+                }
+                //establishing connection
+                conn = new SqlConnection(connectionString);
+                conn.Open();
+
+                //preparing and executing restore query
+                sql = "ALTER DATABASE " + cmbDatabases.Text + " SET SINGLE_USER WITH ROLLBACK IMMEDIATE;";
+                sql += "RESTORE DATABASE " + cmbDatabases.Text + " FROM DISK = '" + txtRestoreFileLoc.Text + "' WITH REPLACE;";
+                command = new SqlCommand(sql, conn);
+                command.ExecuteNonQuery();
+                conn.Close();
+                conn.Dispose();
+                MessageBox.Show("Restoration complete.");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
+//cyclic database backup can be achived by creating proper .bat file and setting Task Scheduler to execute the backup periodically.
